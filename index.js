b6c6a94ade25f214daff389ac7f40b8423631453
Chexwarrior <aplehm@gmail.com>
2018-04-14 08:44:48 -0400

    Initial commit :baby:

---
+++ index.js
@@ -0,0 +1,15 @@
+const puppeteer = require('puppeteer');
+
+(async () => {
+  const browser = await puppeteer.launch({
+    executablePath: 'google-chrome-unstable'
+  });
+
+  const page = await browser.newPage();
+  await page.goto('https://www.google.com');
+  await page.screenshot({
+    path: 'screenshots/google.png'
+  });
+
+  await browser.close();
+})();

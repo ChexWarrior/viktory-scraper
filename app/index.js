d52327f99e6f0e6c014787d8a111059fc1cb18f3
Chexwarrior <aplehm@gmail.com>
2018-04-22 15:07:23 -0400

    Pass game id to script

--- app/index.js
+++ app/index.js
@@ -1,14 +1,24 @@
 const puppeteer = require('puppeteer');
+const process = require('process');
+
+const GAMESBYEMAIL_BASE_URL = 'http://gamesbyemail.com/Games/Play?';
+
+if(process.argv.length < 3) {
+  console.log('You must pass the game ID!');
+  process.exit(1);
+}
+
+let gameID = process.argv[2];
 
 (async () => {
   const browser = await puppeteer.launch({
-    executablePath: 'google-chrome-unstable'
+    executablePath: '/usr/bin/google-chrome'
   });
 
   const page = await browser.newPage();
-  await page.goto('https://www.google.com');
+  await page.goto(`${GAMESBYEMAIL_BASE_URL}${gameID}`);
   await page.screenshot({
-    path: 'google.png'
+    path: 'test.png'
   });
 
   await browser.close();
